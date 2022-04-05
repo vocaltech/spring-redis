2c2e60ebd0446f0efff93a6f695c373c22fa2e5f
vocaltech <it@vocaltech.fr>
2022-04-06 00:21:43 +0200

    Cleanup code

--- src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
+++ src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
@@ -4,22 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RedisApplication {
 	@Bean
-	/*
-	JedisConnectionFactory jedisConnectionFactory() {
-		RedisStandaloneConfiguration redisCfg = new RedisStandaloneConfiguration("localhost", 6379);
-		return new JedisConnectionFactory(redisCfg);
-		return new JedisConnectionFactory();
-	}
-	 */
-
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory();
 	}
@@ -27,7 +17,6 @@ public class RedisApplication {
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		//template.setConnectionFactory(jedisConnectionFactory());
 		template.setConnectionFactory(redisConnectionFactory());
 		return template;
 	}
