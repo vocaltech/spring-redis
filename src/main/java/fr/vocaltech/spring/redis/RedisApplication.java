da988997467864d37786f501cfb568ab4bb41bc5
vocaltech <it@vocaltech.fr>
2022-04-06 23:39:59 +0200

    Remove Redis configuration

--- src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
+++ src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
@@ -2,27 +2,10 @@ package fr.vocaltech.spring.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class RedisApplication {
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
-	}
-
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate() {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory());
-		return template;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
-
 }
