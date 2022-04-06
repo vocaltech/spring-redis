6cb966195afb1e7379dab1f6ca85ec6b29403aa0
vocaltech <it@vocaltech.fr>
2022-04-06 23:38:00 +0200

    Refactor with Optional and lambdas

--- src/test/java/fr/vocaltech/spring/redis/RedisApplicationTests.java
+++ src/test/java/fr/vocaltech/spring/redis/RedisApplicationTests.java
@@ -1,12 +1,18 @@
 package fr.vocaltech.spring.redis;
 
-import fr.vocaltech.spring.redis.models.Position;
-import fr.vocaltech.spring.redis.repositories.PositionRepository;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Optional;
+
+import fr.vocaltech.spring.redis.models.Position;
+import fr.vocaltech.spring.redis.repositories.PositionRepository;
 
 @SpringBootTest
 public class RedisApplicationTests {
@@ -16,8 +22,19 @@ public class RedisApplicationTests {
 	@Test
 	public void whenSavingPosition_thenAvailableOnRetrieval() {
 		Position position = new Position(45.5, 1.5, Instant.now().toEpochMilli(), "track_id", "userId");
-		positionRepository.save(position);
 
-		positionRepository.findAll().forEach(System.out::println);
+		Position savedPos = positionRepository.save(position);
+		String id = savedPos.getId();
+
+		Optional<Position> pos = positionRepository.findById(id);
+
+		pos.ifPresent(v -> assertEquals(45.5, v.getLatitude()));
+		pos.ifPresent(v -> assertEquals(1.5, v.getLongitude()));
+
+		pos.ifPresent(v -> {
+			Instant tsInstant = Instant.ofEpochMilli(v.getTime());
+			String isoTs = DateTimeFormatter.ISO_INSTANT.format(tsInstant);
+			System.out.println("ISO ts: " + isoTs);
+		});
 	}
 }
