a63040647e9677789edb9e1ff6dd4867d967aa50
vocaltech <it@vocaltech.fr>
2022-04-21 23:25:06 +0200

    Implement createPosition()

--- src/main/java/fr/vocaltech/spring/redis/RedisController.java
+++ src/main/java/fr/vocaltech/spring/redis/RedisController.java
@@ -1,12 +1,46 @@
 package fr.vocaltech.spring.redis;
 
+import fr.vocaltech.spring.redis.models.Position;
+import fr.vocaltech.spring.redis.repositories.PositionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.GeoOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RedisController {
+    @Autowired
+    private PositionRepository positionRepository;
+
+    @Autowired
+    private RedisOperations<String, String> operations;
+
+    private GeoOperations<String, String> geoOperations;
+
     @GetMapping("/")
     public String index() {
         return "{ \"message\": \"RedisController\" }";
+    }
+    @PostMapping(
+            path = "positions",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Position> createPosition(@RequestBody Position position) {
+        geoOperations = operations.opsForGeo();
+
+        Position savedPos = positionRepository.save(position);
+        String id = savedPos.getId();
+
+        System.out.println("saved id: " + id);
+
+        return new ResponseEntity<>(savedPos, HttpStatus.CREATED);
     }
 }
