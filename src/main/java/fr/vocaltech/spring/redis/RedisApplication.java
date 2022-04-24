2046b41ceb1d92b2833964b26764955e76bf8cc3
vocaltech <it@vocaltech.fr>
2022-04-24 08:51:53 +0200

    Add ModelMapper bean

--- src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
+++ src/main/java/fr/vocaltech/spring/redis/RedisApplication.java
@@ -1,11 +1,19 @@
 package fr.vocaltech.spring.redis;
 
+import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 }
