0aa1b71f3bd7c7843d578dad684f46616af8ff58
vocaltech <it@vocaltech.fr>
2022-04-27 21:59:11 +0200

    Replace redis-client with redis-smq

---
+++ src/redis-smq/producer.ts
@@ -0,0 +1,19 @@
+import { Message, Producer } from 'redis-smq'
+
+const message = new Message()
+
+message
+    .setBody({ message: 'Hello Redis !'})
+    .setTTL(3600000) // in ms
+    .setQueue('test_queue')
+
+const producer = new Producer()
+producer.produce(message, err => {
+    if (err) {
+        console.log(err)
+    } else {
+        const msgId = message.getId()
+        console.log(`successfully produced msg - id: ${msgId}`)
+        producer.shutdown()
+    }
+})
