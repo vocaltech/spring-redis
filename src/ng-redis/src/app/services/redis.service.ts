d9823415fbcbdedd8bb88927be843c2c98f6c7f5
vocaltech <it@vocaltech.fr>
2022-05-15 12:28:14 +0200

    Implement getPositions()

--- src/ng-redis/src/app/services/redis.service.ts
+++ src/ng-redis/src/app/services/redis.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment'
+import { Position } from '../models/position.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class RedisService {
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getPositions = () => {
+    const url = environment.redis_server_url + '/positions';
+
+    return this.http.get<Position[]>(url)
+  }
 }
