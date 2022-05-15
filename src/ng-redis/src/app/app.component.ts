db8a9340898766973960d6e2772929a7962eb4d3
vocaltech <it@vocaltech.fr>
2022-05-15 12:27:34 +0200

    Add RedisService

--- src/ng-redis/src/app/app.component.ts
+++ src/ng-redis/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { RedisService } from './services/redis.service'
+import { Position } from './models/position.interface';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'ng-redis';
+
+  constructor(private redis: RedisService) {}
+
+  positions: Observable<Position[]> = this.redis.getPositions()
 }
