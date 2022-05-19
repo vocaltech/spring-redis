3132672657007c1b5fb563e064f0b8a7cce43966
vocaltech <it@vocaltech.fr>
2022-05-19 23:16:34 +0200

    Refactor getPositions()

--- src/ng-redis/src/app/services/redis.service.ts
+++ src/ng-redis/src/app/services/redis.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment'
 import { Position } from '../models/position.interface'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,10 @@ export class RedisService {
     private http: HttpClient
   ) {}
 
-  getPositions = () => {
-    const url = environment.redis_server_url + '/positions';
+  getPositions = (): Observable<Position[]> => {
+    const url = environment.redis_server_url + '/api/positions';
 
     return this.http.get<Position[]>(url)
+
   }
 }
