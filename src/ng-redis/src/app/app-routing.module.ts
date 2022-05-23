6461976eee1fb6c00fe8f3f91be5bd4764e5fdaa
vocaltech <it@vocaltech.fr>
2022-05-23 21:49:30 +0200

    Add posform path

--- src/ng-redis/src/app/app-routing.module.ts
+++ src/ng-redis/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PositionFormComponent } from './position-form/position-form.component';
+
+const routes: Routes = [
+  { path: 'posform', component: PositionFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
