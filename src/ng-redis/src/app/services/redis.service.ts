588496a2586930c1a890e62ea26ebbe0d8a2cdd3
vocaltech <it@vocaltech.fr>
2022-05-24 23:12:43 +0200

    Add postPosition function

--- src/ng-redis/src/app/services/redis.service.ts
+++ src/ng-redis/src/app/services/redis.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment'
 import { Position } from '../models/position.interface'
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class RedisService {
+  baseUrl = environment.redis_server_url + '/api/positions';
+
   constructor(
     private http: HttpClient
   ) {}
 
   getPositions = (): Observable<Position[]> => {
-    const url = environment.redis_server_url + '/api/positions';
-
-    return this.http.get<Position[]>(url)
-
+    return this.http.get<Position[]>(this.baseUrl)
   }
+
+  postPosition = (position: Position): Observable<Position> => {
+    return this.http.post<Position>(this.baseUrl, position)
+  }
+
 }
