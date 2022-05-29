cb84aa020f30c86846fa15084c2af688bf8798d5
vocaltech <it@vocaltech.fr>
2022-05-29 12:29:31 +0200

    Implement ngOnInit() to get all positions

--- src/ng-redis/src/app/positions-list/positions-list.component.ts
+++ src/ng-redis/src/app/positions-list/positions-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Position } from '../models/position.interface';
+import { RedisService } from '../services/redis.service'
 
 @Component({
   selector: 'app-positions-list',
   templateUrl: './positions-list.component.html',
   styleUrls: ['./positions-list.component.css']
 })
-export class PositionsListComponent implements OnInit {
 
-  constructor() { }
+export class PositionsListComponent implements OnInit {
+  positions$!: Observable<Position[]>;
+  
+  constructor(private redisService: RedisService) { }
 
   ngOnInit(): void {
+    this.positions$ = this.redisService.getPositions()
   }
-
 }
