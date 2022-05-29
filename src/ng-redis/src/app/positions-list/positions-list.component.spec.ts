f668bf1784aeb4c18f9685d00eccfd729e356d79
vocaltech <it@vocaltech.fr>
2022-05-29 11:38:06 +0200

    Initial commit

---
+++ src/ng-redis/src/app/positions-list/positions-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PositionsListComponent } from './positions-list.component';
+
+describe('PositionsListComponent', () => {
+  let component: PositionsListComponent;
+  let fixture: ComponentFixture<PositionsListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PositionsListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PositionsListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
