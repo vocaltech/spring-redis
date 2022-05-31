78315e79c30c878c44866f3f3bde7cb83448032a
vocaltech <it@vocaltech.fr>
2022-05-31 23:00:59 +0200

    Implement onRemoveAllClick() function

--- src/ng-redis/src/app/positions-list/positions-list.component.ts
+++ src/ng-redis/src/app/positions-list/positions-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { Position } from '../models/position.interface';
@@ -13,9 +14,31 @@ import { RedisService } from '../services/redis.service'
 export class PositionsListComponent implements OnInit {
   positions$!: Observable<Position[]>;
   
-  constructor(private redisService: RedisService) { }
+  constructor(
+    private redisService: RedisService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.positions$ = this.redisService.getPositions()
+  }
+
+  onRemoveAllClick = () => {
+    let result$!: Observable<string>
+
+    result$ = this.redisService.deletePositions()
+
+    result$.subscribe(
+      (data: string) => {
+        // refresh list
+        this.ngOnInit()
+      },
+      (error: any) => {
+        console.log(error)
+      },
+      () => {
+        console.log('deleteAll completed')
+      }
+    )
   }
 }
