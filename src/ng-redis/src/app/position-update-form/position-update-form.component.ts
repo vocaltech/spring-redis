7ad5614f1ec2d6eb9df9d67ee624fdcd23775103
vocaltech <it@vocaltech.fr>
2022-06-04 17:12:02 +0200

    Get position object  from navigation

--- src/ng-redis/src/app/position-update-form/position-update-form.component.ts
+++ src/ng-redis/src/app/position-update-form/position-update-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Position } from '../models/position.interface';
 
 @Component({
   selector: 'app-position-update-form',
   templateUrl: './position-update-form.component.html',
   styleUrls: ['./position-update-form.component.css']
 })
+
 export class PositionUpdateFormComponent implements OnInit {
+  position!: Position;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.position = this.router.getCurrentNavigation()?.extras.state?.position
   }
 
+  ngOnInit(): void {}
 }
