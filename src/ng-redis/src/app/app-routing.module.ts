001d0b9cf200466392c498a3370fefa0a3617850
vocaltech <it@vocaltech.fr>
2022-06-04 11:07:32 +0200

    Add updateform route

--- src/ng-redis/src/app/app-routing.module.ts
+++ src/ng-redis/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PositionFormComponent } from './position-form/position-form.component';
+import { PositionUpdateFormComponent } from './position-update-form/position-update-form.component';
 import { PositionsListComponent } from './positions-list/positions-list.component';
 
 const routes: Routes = [
   { path: '', component: PositionsListComponent },
-  { path: 'posform', component: PositionFormComponent }
+  { path: 'posform', component: PositionFormComponent },
+  { path: 'updateform', component: PositionUpdateFormComponent }
 ];
 
 @NgModule({
