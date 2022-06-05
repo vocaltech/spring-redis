990493ecb15d07668ad6b814f77e5e9a8eb15ad0
vocaltech <it@vocaltech.fr>
2022-06-05 11:36:37 +0200

    Implement onSubmit() new position

--- src/ng-redis/src/app/position-add-form/position-add-form.component.ts
+++ src/ng-redis/src/app/position-add-form/position-add-form.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { RedisService } from '../services/redis.service';
 
 @Component({
   selector: 'app-position-add-form',
   templateUrl: './position-add-form.component.html',
   styleUrls: ['./position-add-form.component.css']
 })
+
 export class PositionAddFormComponent implements OnInit {
+  addForm = this.formBuilder.group({
+    latitude: '',
+    longitude: ''
+  })
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private redisService: RedisService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit = () => {
+    const newPos = {
+      ...this.addForm.value,
+      time: Math.round(Date.now() / 1000),
+      trackId: 'track_1',
+      userId: 'user_1'
+    }
+
+    // post newPos
+    this.redisService.postPosition(newPos).subscribe(
+      (response) => {
+        console.log(response)
+      },
+      (error) => {
+        console.log(error)
+      },
+      () => {
+        console.log('completed')
+
+        // redirection
+        this.router.navigateByUrl('')
+      }
+    )    
   }
-
 }
