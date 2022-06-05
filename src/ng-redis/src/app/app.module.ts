3db4c7a40cc6de7ffe96914d375831c1bedf6552
vocaltech <it@vocaltech.fr>
2022-06-05 11:06:41 +0200

    Add PositionAddFormComponent

--- src/ng-redis/src/app/app.module.ts
+++ src/ng-redis/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PositionFormComponent } from './position-form/position-form.component';
+
 import { PositionsListComponent } from './positions-list/positions-list.component';
 import { PositionUpdateFormComponent } from './position-update-form/position-update-form.component';
+import { PositionAddFormComponent } from './position-add-form/position-add-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PositionFormComponent,
     PositionsListComponent,
-    PositionUpdateFormComponent
+    PositionUpdateFormComponent,
+    PositionAddFormComponent
   ],
   imports: [
     BrowserModule,
