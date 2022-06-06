88038ef6876b3b7aee3cbdba480da4f9841302fd
vocaltech <it@vocaltech.fr>
2022-06-06 10:55:47 +0200

    Import BrowserAnimationsModule

--- src/ng-redis/src/app/app.module.ts
+++ src/ng-redis/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { PositionsListComponent } from './positions-list/positions-list.component';
 import { PositionUpdateFormComponent } from './position-update-form/position-update-form.component';
 import { PositionAddFormComponent } from './position-add-form/position-add-form.component';
@@ -21,7 +21,8 @@ import { PositionAddFormComponent } from './position-add-form/position-add-form.
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
